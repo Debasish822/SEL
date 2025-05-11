e2e52c014516f23c10e58b03ba7243d48906e097
Debasish822 <debadns29@gmail.com>
2025-05-11 12:34:56 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const cacheName = 'joy-buddy-cache-v1';
+const filesToCache = ['/', '/index.html', '/app.js', '/icon.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
